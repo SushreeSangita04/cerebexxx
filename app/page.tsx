f92fe40a128b0393437ab56465889a683f1d8c05
Sushree <sushreesangitaroutaray2004@gmail.com>
2025-08-04 11:20:55 +0530

    start

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React, { use } from 'react'
+import { currentUser } from '@clerk/nextjs/server'
+import Guest from '@/components/Guest';
+export default async function HomePage() {
+  const user=await currentUser();
+  if(!user){
+    return <Guest/>
+  }
+  return (
+    <div>
+      HomePage
+    </div>
+  )
+}
